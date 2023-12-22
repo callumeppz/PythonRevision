09976bf26d5b32ef62285cb0d0521675775b6c0e
Callum Apps <callumapps991@gmail.com>
2023-12-22 20:03:04 +0000

    New Stuff

---
+++ Range.py
@@ -0,0 +1,3 @@
+numbers = range(5,10,2) # range and step
+for number in range(5):
+    print(number)
---
+++ Tuples.py
@@ -0,0 +1,4 @@
+numbers = (1,2,3,3) #tuple
+# tuples are immutable not cahngeable
+numbers.count(3)
+numbers.index(2)
